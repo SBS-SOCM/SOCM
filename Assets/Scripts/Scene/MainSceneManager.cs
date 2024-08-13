476d1a3f32e39dd2981fde8e69a34cd1ff3eabdb
DESKTOP-CCNENP9\songjunmin <thd981227@naver.com>
2024-08-13 20:46:53 +0900

    Set for build

--- Assets/Scripts/Scene/MainSceneManager.cs
+++ Assets/Scripts/Scene/MainSceneManager.cs
@@ -69,7 +69,7 @@ public class MainSceneManager : MonoBehaviour
 
     public void StartGame()
     {
-        GetComponent<SceneManagement>().LoadScene(5);
+        GetComponent<SceneManagement>().LoadScene(0);
     }
     int mod(int x, int m)
     {
